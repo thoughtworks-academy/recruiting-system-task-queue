a1188e3a53b7a80329e70adde4980028da71808a
sialvsic <sialvsic@outlook.com>
2016-04-01 16:49:14 +0800

    Add task controller

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -6,47 +6,62 @@ var yamlConfig = require('node-yaml-config');
 var config = yamlConfig.load('./api/config/config.yml');
 var constant = require('../mixin/constant');
 
+
+function updateTask(req, res) {
+  var uniqId = req.params.uniqId;
+  async.waterfall([
+    (done) => {
+      Task.update(uniqId, req.body).exec(done);
+    },
+    (data, done) => {
+      request.post(data.homeworkURL)
+          .set('Content-Type', 'application/json')
+          .end(done)
+    }
+  ], function (err, data) {
+    res.send(data);
+  });
+}
+
 module.exports = {
-  createTask: function(req, res) {
-		var uniqId;
+  createTask: function (req, res) {
+    var uniqId;
     async.waterfall([
-			(done) => {
-				Task.create({
-		      homeworkURL: req.body.userAnswerRepo,
-		      status: constant.homeworkQuizzesStatus.LINE_UP,
-		      version: req.body.version,
-		      branch: req.branch,
-		      commitTime: Date.parse(new Date()) / constant.time.MILLISECOND_PER_SECONDS
-		    }).exec(done)
-			},
+      (done) => {
+        Task.create({
+          homeworkURL: req.body.userAnswerRepo,
+          status: constant.homeworkQuizzesStatus.LINE_UP,
+          version: req.body.version,
+          branch: req.branch,
+          commitTime: Date.parse(new Date()) / constant.time.MILLISECOND_PER_SECONDS
+        }).exec(done)
+      },
       (result, done) => {
-				uniqId = result.id;
-				var createJobStr = config.CIServer + '/job/' + config.jobName + '/buildWithParameters';
-				var callbackURL = config.taskServer + ':' + config.port + '/tasks/' + uniqId + '/completion';
+        uniqId = result.id;
+        var createJobStr = config.CIServer + '/job/' + config.jobName + '/buildWithParameters';
+        var callbackURL = config.taskServer + ':' + config.port + '/tasks/' + uniqId + '/completion';
 
         request
-          .post(createJobStr)
-          .set('Content-Type', 'application/json')
-          .query({
-            USER_REPO: req.body.userAnswerRepo,
-            CALLBACK_URL: callbackURL,
-            BRANCH: req.body.branch,
-            EVALUATE_SCRIPT_URL: config.nginxServer + req.body.evaluateScript
-          })
-          .end(done);
+            .post(createJobStr)
+            .set('Content-Type', 'application/json')
+            .query({
+              USER_REPO: req.body.userAnswerRepo,
+              CALLBACK_URL: callbackURL,
+              BRANCH: req.body.branch,
+              EVALUATE_SCRIPT_URL: config.nginxServer + req.body.evaluateScript
+            })
+            .end(done);
       }
-    ], (err, res) => {
-			if (err) {
-				res.sendStatus(constant.httpCode.INTERNAL_SERVER_ERROR);
-			} else {
-				res.send({
-					status: constant.httpCode.OK,
-					uniqId: uniqId
-				});
-			}
+    ], (err, data) => {
+      if (err) {
+        res.sendStatus(constant.httpCode.INTERNAL_SERVER_ERROR);
+      } else {
+        res.send({
+          status: constant.httpCode.OK,
+          uniqId: uniqId
+        });
+      }
     })
   },
-  result: function(req, res) {
-  	
-  }
+  update: updateTask
 };
